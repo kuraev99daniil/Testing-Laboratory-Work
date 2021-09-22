e78c0e419dc5131ef0d607644f02b7661b20109a
kuraev99daniil <kuraev.dm99@gmail.com>
2021-09-23 00:35:37 +0300

    Init project

---
+++ Laboratory-Work-One/Laboratory-Work-One/NUnitTesting.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Laboratory_Work_One
+{
+    [TestFixture]
+    public class NUnitTesting
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+    }
+}
---
+++ Laboratory-Work-One/Laboratory-Work-One/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Laboratory-Work-One")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laboratory-Work-One")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4794d1c3-28a1-4a50-a142-35f893dcfbfc")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
